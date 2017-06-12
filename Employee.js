fe584f78236029c542d2ba415c38d8eb46e250af
Sang Cho <Sang Cho>
2017-06-12 14:47:03 -0500

    done with assignment, probably not the prettiest code...

--- Employee.js
+++ Employee.js
@@ -2,13 +2,49 @@
 
 const fs = require('fs')
 
+
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.name=name
+    this.title=title
+    this.salary=salary
+  }
+  get empoyleeName(){
+    return this.name
+  }
+  set employeeName(name){
+    return this.name=name
   }
 
-  // TODO ???
+  get employeetitle(){
+    return this.title
+  }
+  set employeeTitle(title){
+    return this.title=title
+  }
+
+  get employeeSalary(){
+    return this.salary
+  }
+  set employeeSalary(salary){
+    return this.salary=salary
+  }
+
+  promote(title, salary){
+    this.title=title
+    this.salary=salary
+  }
+
+  static parseFromFilePath(path){
+      let stuff=JSON.parse(fs.readFileSync(path))
+      let leEmployeeName=stuff.name
+      let leEmployeeTitle=stuff.title
+      let leEmployeeSalary=stuff.salary
+      let leEmployee=new Employee(leEmployeeName,leEmployeeTitle,leEmployeeSalary)
+      return leEmployee
+    }
 }
+
 
 // TODO ???
 
